e97e3f371c9485ca7d2e06ce81c97d888da21d02
lucasdwn <lfcosta0804@gmail.com>
2025-02-11 19:14:24 -0300

    Initial commit portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    // Toggle para abrir e fechar a sidebar
+    $("#sidebar-toggle").click(function () {
+        $(".sidebar").toggleClass("open");
+
+        // Altera o ícone de hamburger para o ícone de fechar
+        if ($(".sidebar").hasClass("open")) {
+            $(this).find("i").removeClass("fa-bars").addClass("fa-times"); // Ícone de fechar
+        } else {
+            $(this).find("i").removeClass("fa-times").addClass("fa-bars"); // Ícone de hamburger
+        }
+    });
+
+    // Ao clicar em um link de navegação
+    $(".nav-link").click(function () {
+        let pageId = $(this).data("page");
+
+        // Alterando o conteúdo
+        $(".page").removeClass("active");
+        $("#" + pageId).addClass("active");
+
+        // Fechar a sidebar se estiver aberta (apenas para dispositivos móveis)
+        if ($(window).width() <= 768) {
+            $(".sidebar").removeClass("open");  // Fecha a sidebar
+            $("#sidebar-toggle i").removeClass("fa-times").addClass("fa-bars"); // Restaura o ícone de hamburger
+        }
+    });
+
+    function calcularIdade(dataNascimento) {
+        let hoje = new Date();
+        let nascimento = new Date(dataNascimento);
+        let idade = hoje.getFullYear() - nascimento.getFullYear();
+        let mesAtual = hoje.getMonth();
+        let mesNascimento = nascimento.getMonth();
+        let diaAtual = hoje.getDate();
+        let diaNascimento = nascimento.getDate();
+
+        if (mesAtual < mesNascimento || (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+            idade--;
+        }
+
+        return idade;
+    }
+
+    let dataNascimento = "2003-04-08"; 
+    $("#age").text(calcularIdade(dataNascimento));
+});
